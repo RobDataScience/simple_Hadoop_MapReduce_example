fe89c3a5945ca0e4a915c5d1c81b57c385a89c25
Robelasmare <112607325+RobDataScience@users.noreply.github.com>
2022-09-02 19:09:56 -0600

    Update mapper.py

--- mapper.py
+++ mapper.py
@@ -1,20 +1,13 @@
 #!/usr/bin/env python
 import sys
-
 # get all lines from stdin
 for line in sys.stdin:
     # remove leading and trailing whitespace
     line = line.strip()
-
-    # make lower case
-    line = line.lower()
-
+   
     # split the line into words; splits on any whitespace
     words = line.split()
-    
-    # notice the loop is different
 
+    # output tuples (word, 1) in tab-delimited format
     for word in words:
-        if word not in stopwords:
-         print '%s\t%s' % (word, "1")
-
+        print '%s\t%s' % (word, "1")
